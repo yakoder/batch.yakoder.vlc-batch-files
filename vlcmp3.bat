918bc0baa9c7852aabadfbe8958da1827b2bc564
Jon Warren <jon@jonwarren.info>
2016-06-05 15:47:35 -0500

    Updated with current 'vlcmp3.bat'

--- vlcmp3.bat
+++ vlcmp3.bat
@@ -1,37 +1,99 @@
 @echo off
+rem -----------------------------------------------------------------
+rem Conversion script to extract MP3 audio from MP4 video files
+rem using VLC.
+rem -----------------------------------------------------------------
+
+rem -----------------------------------------------------------------
+rem IMPORTANT!!! Set VLC_HOME to the directory containing vlc.exe
+rem -----------------------------------------------------------------
 set VLC_HOME=C:\PortableApps\PortableApps\VLCPortable\App\vlc
-set LOG_PATH=C:\PortableApps\tmp
-set BAD_LOG=%LOG_PATH%\vlcmp3-bad.log
+rem -----------------------------------------------------------------
+
+rem -----------------------------------------------------------------
+rem Temp files for use in conversion
+rem Defaults to directory in which this batch file is called
+rem May be changed, if desired
+rem -----------------------------------------------------------------
+set INPUT=in.mp4
+set OUTPUT=out.mp3
+rem -----------------------------------------------------------------
+
+rem -----------------------------------------------------------------
+rem Start of normal processing
+rem -----------------------------------------------------------------
+rem Clean-up from previous run
+if exist "%INPUT%" (
+    @echo.
+    @echo WARNING! "%INPUT%" exists, deleting.
+    @echo.
+    del /q /f "%INPUT%"
+)
+if exist "%OUTPUT%" (
+    @echo.
+    @echo WARNING! "%OUTPUT%" exists, deleting.
+    @echo.
+    del /q /f "%OUTPUT%"
+)
 
 set SOURCE="%~1"
+rem Handle any special characters
 setlocal enabledelayedexpansion
+rem Remove extra quotes
 set SOURCE="!SOURCE:~1,-1!"
-set OUTPUT="!SOURCE:.mp4=.mp3!"
-set OUTPUT=!OUTPUT:~1,-1!
-set BACKUP="!OUTPUT:.mp3=.old.mp3!"
-set BACKUP=!BACKUP:~1,-1!
+rem Create temp-input file to avoid dealing further with special characters
+copy /v /y /b !SOURCE! "!INPUT!"
+if not exist "!INPUT!" (
+    @echo.
+    @echo ERROR^^! Cannot proceed: creation of "!INPUT!" from !SOURCE! failed^^!
+    @echo.
+    goto:eof
+)
 
-if exist "!OUTPUT!" (
-    if exist "!BACKUP!" (
+rem Create one backup of the intended target, just in case of re-run
+if exist !TARGET! (
+    if exist !BACKUP! (
         @echo.
-        @echo ERROR! Cannot run: "!OUTPUT!" exists with 1 backup
+        @echo ERROR^^! Cannot run: !TARGET! exists with 1 backup
+        @echo.
         goto:eof
     ) else (
         @echo.
-        @echo WARNING! "!OUTPUT!" exists renaming to "!BACKUP!"
-        ren "!OUTPUT!" "!BACKUP!"
+        @echo WARNING^^! !TARGET! exists, moving to !BACKUP!
+        @echo.
+        move /y !TARGET! !BACKUP!
     )
 )
 
 rem Run VLC...
-"%VLC_HOME%\vlc.exe" --qt-start-minimized --no-qt-privacy-ask "!SOURCE:~1,-1!" :sout=#transcode{vcodec=none,acodec=mp3,ab=224,channels=2,samplerate=44100}:std{access=file{no-overwrite},mux=wav,dst=!OUTPUT! } vlc://quit
+"%VLC_HOME%\vlc.exe" --qt-start-minimized --no-qt-privacy-ask !INPUT! :sout=#transcode{vcodec=none,acodec=mp3,ab=224,channels=2,samplerate=44100}:std{access=file{no-overwrite},mux=wav,dst=!OUTPUT! } vlc://quit
+
+rem Post-run clean-up of temp-input file
+if exist "%INPUT%" (
+    del /q /f "%INPUT%"
+)
+
+rem Rename temp-output file to target name
+if exist "!OUTPUT!" (
+    move /y "!OUTPUT!" !TARGET!
+) else (
+    @echo.
+    @echo WARNING^^! "!OUTPUT!" does not exist^^!
+    @echo          VLC did not convert !SOURCE! successfully.
+    @echo.
+)
 
 goto:eof
+rem -----------------------------------------------------------------
+rem End of normal processing
+rem -----------------------------------------------------------------
 
 rem -----------------------------------------------------------------
 :USEERR
 @echo.
-@echo ERROR! Usage: %0 [/f] SOURCE.mp4 [/f]
+@echo ERROR^^! Usage: %0 [/f] SOURCE.mp4 [/f]
 @echo    /f  optional force deletion of existing backup file
+@echo.
 goto:eof
+rem -----------------------------------------------------------------
 
